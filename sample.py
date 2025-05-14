e943b9ad9d9f7431dc172cfa93763d5a7a600451
M-Jafarkhani <mahdi.jafarkhani@gmail.com>
2025-05-14 16:23:38 +0200

    Change the code

--- sample.py
+++ sample.py
@@ -16,7 +16,7 @@ def fibunatchi(n):
         return [0, 1]
     
     fib_sequence = [0, 1]
-    for i in range(2, n):
+    for i in range(3, n):
         next_value = fib_sequence[-1] + fib_sequence[-2]
         fib_sequence.append(next_value)
     
